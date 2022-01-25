e7a06c5bdcbd46404b53c02126084151f1d84c12
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-01-25 12:45:03 +0530

    Updated Banner layout

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -1,8 +1,21 @@
 <section class="banner">
-	<div data-aos="fade-in">
-		<div class="banner">
-			<img src="{{ asset('assets/img/postal-banner.JPG') }}" alt="" class="img-fluid"> 
-		</div>
+    <div data-aos="fade-in">
+        <div class="banner bg-custom">
+            {{-- <img src="{{ asset('assets/img/postal-banner.JPG') }}" alt="" class="img-fluid"> --}}
+        </div>
 
-	</div>
+    </div>
+    <style>
+        .bg-custom {
+            background-image: url({{ asset('assets/img/postal-banner.JPG') }});
+            background-size: cover;
+			background-position: center;
+            background-repeat: no-repeat;
+            width: 100%;
+            height: 0;
+            padding-top: 25%
+
+        }
+
+    </style>
 </section><!-- End Hero -->
