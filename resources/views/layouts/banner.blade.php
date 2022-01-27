d0884647bbd65481457397503222d9e51362a38f
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-01-27 11:37:39 +0530

    Updated Notification scroll content

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -8,10 +8,7 @@
     <div class="notification">
         <marquee direction="left">
             <ul>
-                <li><a href="#">Virtual Philately Exhibition on 12/12/2021 Click Here to visit***</a></li>
-                <li><a href="#">Virtual Philately Exhibition on 12/12/2021 Click Here to visit***</a></li>
-                <li><a href="#">Virtual Philately Exhibition on 12/12/2021 Click Here to visit***</a></li>
-                <li><a href="#">Virtual Philately Exhibition on 12/12/2021 Click Here to visit***</a></li>
+                <li><a href="#">Virtual Philately Exhibition by Guwahati Division from ___________ Click Here to visit***</a></li>
             </ul>
         </marquee>
     </div>
