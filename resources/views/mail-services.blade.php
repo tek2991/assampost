991edc2af3a01f5f1f20c676b8f6c1f12c9f77d9
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-01-27 12:02:34 +0530

    Updated Content

--- resources/views/mail-services.blade.php
+++ resources/views/mail-services.blade.php
@@ -445,9 +445,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Letter</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">A Letter means any communication, which is enclosed in an envelope and addressed.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/Letter.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
@@ -455,9 +454,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Book Packet</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">Shall not contain any paper money, postage or other stamps, Cheque​ etc</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/Book-Packet.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
@@ -465,9 +463,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Parcel</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">Anything can be sent in a parcel except articles whose transmission is prohibited.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/Parcel.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
@@ -475,9 +472,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Reg Newspaper</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">Registered​​ News paper for publication consisting wholly or in great part of political or other news.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/Registered-Newspaper.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
@@ -493,9 +489,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">International Letters</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">India Post provides international postal services and can be booked in all the Departmental Post offices across the country.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/International-Letter.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
 
@@ -504,9 +499,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">EMS Speed Post</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">International Speed Post (EMS), the premium and time bound intern​ational postal service for documents and merchandise.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/EMS-SpeedPost.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
@@ -514,9 +508,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Air Parcels</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">This service is a dedicated ser vice for corporate and retail customers to provide economical and fast merchandise services.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/International-Air-Parcels.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
@@ -524,9 +517,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Tracked Packet</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">International Tracked Packet is specially designed to cater to​ the needs of eCommerce for cross border transactions. </p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/International-Tracked-Packet-.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
@@ -542,9 +534,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Speed Post</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">Speed Post is available for both domestic and International mail.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/Speed-Post.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
 
@@ -553,9 +544,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Busioness Parcel</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">Business Parcel is a premium surface service to suit the requirements of business customers for an economical and reliable distribution solution.​</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/Business-Parcel-New.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
@@ -563,19 +553,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Logistics Post</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <div class="card service-card-1">
-                            <div class="card-body">
-                                <h5 class="card-title">Insurance</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">Logistics Post provides business customers a cost-effective and efficient solution.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/Logistic-Post-New.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
@@ -591,9 +570,8 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">Book Now Pay Later</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">Bulk customer is eligible for credit facility provided he enters in to an agreement with the Department.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/BNPL.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
 
@@ -602,19 +580,17 @@
                         <div class="card service-card-1">
                             <div class="card-body">
                                 <h5 class="card-title">COD Option</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <p class="card-text">In order to meet the requirement of bulk customers for collection of amount, Cash on Delivery facility is available.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/COD.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
                     <div class="col-md-3">
                         <div class="card service-card-1">
                             <div class="card-body">
-                                <h5 class="card-title">Corporate Account</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up
-                                    the bulk of the card's content.</p>
-                                <a class="black-btn" href="#"> Details </a>
+                                <h5 class="card-title">Volume Discounts</h5>
+                                <p class="card-text">For corporate customers and regular users, Speed Post provides customised solutions for your needs.</p>
+                                <a class="black-btn" href="https://www.indiapost.gov.in/MBE/Pages/Content/Speed-Post-Discount-Structures.aspx" target="_blank"> Details </a>
                             </div>
                         </div>
                     </div>
