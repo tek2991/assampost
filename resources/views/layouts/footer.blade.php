b07243cdb2cfdcc2b3bfcbae3871d7218841396f
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-02 13:24:55 +0530

    Updated footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,76 +2,88 @@
 
 <footer id="footer">
 
-	<div class="footer-top">
-		<div class="container">
-			<div class="row">
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
 
 
 
-				<div class="col-lg-2 col-md-6 footer-links">
-					<h4>Services</h4>
-					<ul>
-						<li><a href="#">Registered</a></li>
-						<li><a href="#">Speed Post</a></li>
-						<li><a href="#">Logistics</a></li>
-						<li><a href="#">Marketing</a></li>
-						<li><a href="#">BNPL</a></li>
-					</ul>
-				</div>
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Services</h4>
+                    <ul>
+                        <li><a href="https://www.indiapost.gov.in/MBE/Pages/Content/Letter.aspx"
+                                target="_blank">Letter</a></li>
+                        <li><a href="https://www.indiapost.gov.in/MBE/Pages/Content/Speed-Post.aspx"
+                                target="_blank">Speed Post</a></li>
+                        <li><a href="https://www.indiapost.gov.in/MBE/Pages/Content/Logistic-Post-New.aspx"
+                                target="_blank">Logistics</a></li>
+                        <li><a href="https://www.indiapost.gov.in/MBE/Pages/Content/Parcel.aspx"
+                                target="_blank">MParcel</a></li>
+                        <li><a href="https://www.indiapost.gov.in/MBE/Pages/Content/BNPL.aspx" target="_blank">BNPL</a>
+                        </li>
+                    </ul>
+                </div>
 
 
-				<div class="col-lg-3 col-md-6 footer-links">
-					<h4>Banking and Insurance</h4>
-					<ul>
-						<li><a href="#">Post Office savings acount</a></li>
-						<li><a href="#">India Post payment bank</a></li>
-						<li><a href="#">Postal Life Insurance</a></li>
-					</ul>
-				</div>
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Banking and Insurance</h4>
+                    <ul>
+                        <li><a href="https://www.indiapost.gov.in/Financial/Pages/Content/Post-Office-Saving-Schemes.aspx"
+                                target="_blank">Post Office savings acount</a></li>
+                        <li><a href="https://www.ippbonline.com/" target="_blank">India Post payment bank</a></li>
+                        <li><a href="https://www.indiapost.gov.in/Financial/Pages/Content/pli.aspx"
+                                target="_blank">Postal Life Insurance</a></li>
+                        <li><a href="https://www.indiapost.gov.in/Financial/Pages/Content/rpli.aspx"
+                                target="_blank">Rural PLI</a></li>
+                    </ul>
+                </div>
 
-				<div class="col-lg-2 col-md-6 footer-links">
+                {{-- <div class="col-lg-2 col-md-6 footer-links">
 					<h4>Others</h4>
 					<ul>
 						<li><a href="#">MIS</a></li>
 						<li><a href="#">Adhar Service</a></li>
 						<li><a href="#">CSC</a></li>
 					</ul>
-				</div>
-
-				<div class="col-lg-2 col-md-6 footer-links">
-					<h4>Others</h4>
-					<ul>
-						<li><a href="#">MIS</a></li>
-						<li><a href="#">Adhar Service</a></li>
-						<li><a href="#">CSC</a></li>
-					</ul>
-				</div>
-
-
-				{{-- <div class="col-lg-3 col-md-3 footer-newsletter">
-
-					<div class="social-links text-center text-lg-right pt-3 pt-lg-0">
-						<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-						<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-						<a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-					</div>
-
 				</div> --}}
 
-			</div>
-		</div>
-	</div>
+                <div class="col-lg-3 col-md-6 footer-links">
+                    <h4>Others</h4>
+                    <ul>
+                        <li><a href="https://www.indiapost.gov.in/MBE/Pages/Content/POPSK.aspx" target="_blank">Passport
+                                Seva Kendra</a></li>
+                        <li><a href="https://www.indiapost.gov.in/MBE/Pages/Content/AadhaarUpdationCentres.aspx"
+                                target="_blank">Adhar Service</a></li>
+                        <li><a href="https://www.indiapost.gov.in/MBE/Pages/Content/IPPRS.aspx" target="_blank">PRS</a>
+                        </li>
+                    </ul>
+                </div>
 
-	<div class="container d-lg-flex py-2">
-		<div class="me-lg-auto text-center text-lg-start">
-			<div class="copyright">
-				&copy; Copyright. All Rights Reserved
-			</div>
-		</div>
-	</div>
+
+                <div class="col-lg-3 col-md-3 footer-newsletter">
+
+                    <div class="social-links text-center text-lg-right pt-3 pt-lg-0">
+                        <a href="https://twitter.com/cpmgassam" target="_blank" class="twitter"><i
+                                class="bx bxl-twitter"></i></a>
+                        <a href="https://www.facebook.com/cpmgassam/" target="_blank" class="facebook"><i
+                                class="bx bxl-facebook"></i></a>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container d-lg-flex py-2">
+        <div class="me-lg-auto text-center text-lg-start">
+            <div class="copyright">
+                &copy; Copyright. All Rights Reserved
+            </div>
+        </div>
+    </div>
 
 </footer>
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+        class="bi bi-arrow-up-short"></i></a>
 <!-- End Footer -->
-
-
