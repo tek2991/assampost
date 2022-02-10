119149808bd7ce432fd9ed2c63d8bd797de22384
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-10 11:15:49 +0530

    Added latitude and longitude to offices

--- app/Models/Office.php
+++ app/Models/Office.php
@@ -5,10 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Office extends Model
 {
     use HasFactory;
     use SoftDeletes;
-    protected $fillable = ['title', 'address_line1', 'address_line2', 'phone_no', 'email', 'website', 'other_description', 'is_active'];
-    
+    protected $fillable = ['title', 'address_line1', 'address_line2', 'phone_no', 'email', 'website', 'other_description', 'is_active', 'division_id', 'district_id'];
+
+    public function otherOffices(){
+        return $this->hasMany(OtherOffice::class);
+    }
+
+    public function division(){
+        return $this->belongsTo(Division::class);
+    }
+
+    public function district(){
+        return $this->belongsTo(District::class);
+    }
 }
--- app/Models/OtherOffice.php
+++ app/Models/OtherOffice.php
@@ -9,9 +9,19 @@ class OtherOffice extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
     protected $with = ['office'];
-    protected $fillable = ['office_id','title', 'address_line1', 'address_line2', 'phone_no', 'email', 'website', 'other_description', 'is_active'];
+    protected $fillable = ['office_id','title', 'address_line1', 'address_line2', 'phone_no', 'email', 'website', 'other_description', 'is_active', 'division_id', 'district_id'];
+
     public function office(){
-        return $this->belongsTo('App\Models\Office');
+        return $this->belongsTo(Office::class);
+    }
+
+    public function division(){
+        return $this->belongsTo(Division::class);
+    }
+
+    public function district(){
+        return $this->belongsTo(District::class);
     }
 }
