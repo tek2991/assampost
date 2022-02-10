f129f1cd8419984beb07e9aabd55ab84fedf9211
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-10 12:47:42 +0530

    Updated Office view

--- app/Http/Controllers/OfficeController.php
+++ app/Http/Controllers/OfficeController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Office;
+use App\Models\District;
+use App\Models\Division;
 use Illuminate\Http\Request;
 
 class OfficeController extends Controller
 {
     public function index(Request $request){
+        
         $offices = Office::paginate(10);
-        return view('administrative-office', compact('offices'));
+        $districts = District::all();
+        $divisions = Division::all();
+        return view('administrative-office', compact('offices', 'districts', 'divisions'));
     }
 }
--- resources/views/administrative-office.blade.php
+++ resources/views/administrative-office.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('title')
-Administrative Office
+    Administrative Office
 @endsection
 @section('content')
 
@@ -9,35 +9,103 @@ Administrative Office
 
 
             <div class="row">
-                <div class="col-md-12">
-                @forelse ($offices as $office)
-                    <div class="card address-card-1">
-                        <div class="card-header">
-                            <h5>{{$office->title}}</h5>
-                        </div>
+                <div class="col-md-3">
+                    <form action="{{ route('view-administrative-office') }}" method="GET">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5>Filters</h5>
+                            </div>
 
-                        <div class="card-body">
-                            <p class="card-text">
-                                {{$office->address_line1}} <br />
-                                 {{$office->address_line2}}<br />
-                                Ph No. {{$office->phone_no}} <br />
-                                Email: {{$office->email}}
-                            </p>
-                        </div>
-                    </div>
-                @empty
-                    <div class="alert alert-danger">
-                        No Office Found
-                    </div>
-                @endforelse
-                    
+                            <div class="card-body">
 
-                   
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <label for="str" style="padding-top:5px">Search</label>
+                                        </div>
+                                        <div class="col-md-9">
+                                            <input type="text" class="form-control" name="str" id="str"
+                                                placeholder="name or Pincode">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group" style="margin-top:1rem">
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <label for="division_id" style="padding-top:5px">Division</label>
+                                        </div>
+                                        <div class="col-md-9">
+                                            <select name="division_id" class="form-control" id="division_id">
+                                                <option value="">Select Division</option>
+                                                @foreach ($divisions as $division)
+                                                    <option value="{{ $division->id }}">
+                                                        {{ $division->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group" style="margin-top:1rem">
+                                    <div class="row">
+                                        <div class="col-md-3">
+                                            <label for="district_id" style="padding-top:5px">District</label>
+                                        </div>
+                                        <div class="col-md-9">
+                                            <select name="district_id" class="form-control" id="district_id">
+                                                <option value="">Select district</option>
+                                                @foreach ($districts as $district)
+                                                    <option value="{{ $district->id }}">{{ $district->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group" style="margin-top:1rem">
+                                    <div class="row">
+                                        <div class="col-md-12 text-center">
+                                            <button type="submit" name="submit" class="btn btn-success btn-icon-split">
+                                                <span class="icon text-white-50">
+                                                    <i class="fas fa-check"></i>
+                                                </span>
+                                                <span class="text">Submit</span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="col-md-9">
+                    @forelse ($offices as $office)
+                        <div class="card address-card-1">
+                            <div class="card-header">
+                                <h5>{{ $office->title }}</h5>
+                            </div>
+
+                            <div class="card-body">
+                                <p class="card-text">
+                                    {{ $office->address_line1 }} <br />
+                                    {{ $office->address_line2 }}<br />
+                                    Ph No. {{ $office->phone_no }} <br />
+                                    Email: {{ $office->email }}
+                                </p>
+                            </div>
+                        </div>
+                    @empty
+                        <div class="alert alert-danger">
+                            No Office Found
+                        </div>
+                    @endforelse
+
+
+
                 </div>
 
 
             </div>
-            {{$offices->links()}} 
+            {{ $offices->links() }}
         </div>
 
 
