6bce357a578556700e432eff237667555fc3358d
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-10 10:51:57 +0530

    Added Districts migration, model and seeder

---
+++ database/seeders/DistrictSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DistrictSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $districts = [
+            'Baksa', 'Barpeta', 'Biswanath', 'Bongaigaon', 'Cachar', 'Charaideo', 'Chirang', 'Darrang', 'Dhemaji', 'Dhubri', 'Dibrugarh', 'Dima Hasao', 'Goalpara', 'Golaghat', 'Hailakandi', 'Hojai', 'Jorhat', 'Kamrup Metropolitan', 'Kamrup', 'Karbi Anglong', 'Karimganj', 'Kokrajhar', 'Lakhimpur', 'Majuli', 'Morigaon', 'Nagaon', 'Nalbari', 'Sivasagar', 'Sonitpur', 'South Salmara-Mankachar', 'Tinsukia', 'Udalguri', 'West Karbi'
+        ];
+
+        $insert_data = [];
+
+        foreach ($districts as $district) {
+            $insert_data[] = [
+                'name' => $district,
+                'created_at' => now(),
+                'updated_at' => now()
+            ];
+        }
+
+        DB::table('districts')->insert($insert_data);
+    }
+}
