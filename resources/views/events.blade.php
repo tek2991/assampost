2845f6b8ff3edbeb8f018a31542a76afc39b073d
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-16 13:21:23 +0530

    Updated Event views

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,31 +1,26 @@
 @extends('layouts.app')
 @section('title')
-Events
+    Events
 @endsection
 @section('content')
     <section class="conact admin">
         <div class="container">
+            <div class="row mb-4">
+                <h3 class="section-header">Events</h3>
+            </div>
             <div class="services-wrap">
                 <div class="row">
                     @forelse ($events as $event)
                         <div class="col-md-4">
-                        <div class="card event-card-1">
-
-                            <div class="card-header">
-                                <img class="img-fluid" src="{{$event->picture}}" alt="{{ $event->title}}">
-                            </div>
-
-                            <div class="card-body">
-
-                                <h3> {{ $event->title}}</h3>
-
-                                <p>{!! $event->brief_description !!}</p>
-
-                                <a href="{{route('event.detail',$event->slug)}}" class="black-btn"> Details</a>
-
+                            <div class="card" style="width: 18rem;">
+                                <img src="{{ $event->picture }}" class="card-img-top" alt="{{ $event->title }}">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $event->title }}</h5>
+                                    <p class="card-text">{!! $event->brief_description !!}</p>
+                                    <a href="{{ route('event.detail', $event->slug) }}" class="btn black-btn">Details</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
                     @empty
                         <div class="col-md-12">
                             <div class="card event-card-1">
@@ -34,13 +29,10 @@ Events
                                 </div>
                             </div>
                         </div>
-                        
                     @endforelse
-                    
-                    
+                    {{ $events->links() }}
                 </div>
             </div>
-            {{$events->links()}}
         </div>
     </section>
 @endsection
