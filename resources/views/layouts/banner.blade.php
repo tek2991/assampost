57fc7b277b5c9c02c55904fcee068b764c88ecd8
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-16 12:36:54 +0530

    Updated banner

--- resources/views/layouts/banner.blade.php
+++ resources/views/layouts/banner.blade.php
@@ -2,38 +2,42 @@
     <div data-aos="fade-in">
         <div id="image-slider" class="splide">
             <div class="splide__track">
-                    <ul class="splide__list">
-                        @php
-                            $banners = DB::table('banners')->where('is_active',1)->get();
-                        @endphp
-                        @foreach($banners as $banner)
+                <ul class="splide__list">
+                    @php
+                        $banners = DB::table('banners')
+                            ->where('is_active', 1)
+                            ->get();
+                    @endphp
+                    @foreach ($banners as $banner)
                         <li class="splide__slide">
-                            @if($banner->url != '')
-                            <a href="{{$banner->url}}" target="_blank">
+                            @if ($banner->url != '')
+                                <a href="{{ $banner->url }}" target="_blank">
                             @endif
-                            <img src="{{$banner->banner_image}}">
-                            @if($banner->url != '')
-                            </a>
+                            <img src="{{ $banner->banner_image }}">
+                            @if ($banner->url != '')
+                                </a>
                             @endif
                         </li>
-                        @endforeach
-                    </ul>
+                    @endforeach
+                </ul>
             </div>
-            </div>
+        </div>
 
     </div>
     <div class="notification">
         <marquee direction="left">
             <ul>
-                        @php
-                            $notices_front = DB::table('notices')->where('is_active',1)->where('publish_to_scroll',1)->get();
-                        @endphp
-                  @foreach($notices_front as $n)      
-                <li><a href="{{ $n->file_path }}" target="_blank">{{ $n->title }} </a></li>
+                @php
+                    $notices_front = DB::table('notices')
+                        ->where('is_active', 1)
+                        ->where('publish_to_scroll', 1)
+                        ->get();
+                @endphp
+                @foreach ($notices_front as $n)
+                    <li><a href="{{ $n->file_path }}" target="_blank">{{ $n->title }} </a></li>
                 @endforeach
-                <li><a href="{{ url('/exhibition/guwahati-exhibition') }}">Virtual Philately Exhibition by Guwahati Division from ___________ Click Here to visit***</a></li>
             </ul>
         </marquee>
     </div>
-    
+
 </section>
