8705a8fd41a49ba0241508bacaedbef41031e59e
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-17 11:41:17 +0530

    Updated Views

--- resources/views/admin/office/index.blade.php
+++ resources/views/admin/office/index.blade.php
@@ -3,63 +3,55 @@
     {{ __('Create Administrative Office') }}
 @endsection
 @section('breadcrumb')
-<div class="d-sm-flex align-items-center justify-content-between mb-4">
-<h1 class="h3 mb-0 text-gray-800">Administrative Office</h1>
-</div>
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Administrative Office</h1>
+    </div>
 @endsection
 @section('content')
- <div class="row">
- @if(Session::has('success'))
-<div class="container">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="alert {{ Session::get('alert-class', 'alert-success') }}">
-                {{-- <i class="fe fe-check mr-2" aria-hidden="true"></i> --}}
-                <button type="button" class="close" data-dismiss="alert"></button>
-                {!! Session::get('success') !!}
-            </div>
+    @if (Session::has('success'))
+        <div class="alert {{ Session::get('alert-class', 'alert-success') }}">
+            <button type="button" class="close" data-dismiss="alert"></button>
+            {!! Session::get('success') !!}
         </div>
-    </div>
-</div>
-@endif
- @if ($errors->any())
-    @foreach ($errors->all() as $error)
-        <div class="alert alert-danger">{{$error}}</div>
-    @endforeach
- @endif
- @foreach ($offices as $k=>$office)
-    <div class="col-md-3">
-        <div class="card position-relative  py-3  border-left-danger">
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h5 class="card-title text-center">{{$office->title}}</h5>
+    @endif
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <div class="alert alert-danger">{{ $error }}</div>
+        @endforeach
+    @endif
+    <div class="row">
+        @foreach ($offices as $k => $office)
+            <div class="col-md-3">
+                <div class="card position-relative  py-3  border-left-danger">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h5 class="card-title text-center">{{ $office->title }}</h5>
+                            </div>
+                        </div>
+                        <p>{{ $office->address_line1 }}</p>
+                        <p>{{ $office->address_line2 }}</p>
+                        <p>{{ $office->phone_no }}</p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <a href="{{ route('admin.office.edit', $office->id) }}"
+                                    class="btn btn-primary btn-block btn-sm"><i class="fa fa-edit"></i>
+                                </a>
+                            </div>
+                            <div class="col-md-6">
+                                <form action="{{ route('admin.office.destroy', $office->id) }}" method="Post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger btn-block  btn-sm"
+                                        onclick="return confirm('Are you sure to delete?')"><i class="fa fa-trash"></i>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <p>{{$office->address_line1}}</p>
-                <p>{{$office->address_line2}}</p>
-                <p>{{$office->phone_no}}</p>
-                <div class="row">
-                    <div class="col-md-6">
-                        <a href="{{route('admin.office.edit',$office->id)}}" class="btn btn-primary btn-block btn-sm"><i class="fa fa-edit"></i></a>
-                    </div>
-                    <div class="col-md-6">
-                        <form action="{{route('admin.office.destroy',$office->id)}}" method="Post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger btn-block  btn-sm" onclick="return confirm('Are you sure to delete?')"><i class="fa fa-trash"></i></button>
-                        </form>
-                    </div>
-                </div>
-                
-               
             </div>
-         </div>
+        @endforeach
     </div>
-@endforeach
-
- </div> 
- {{$offices->links()}}          
+    {{ $offices->links() }}
 @endsection
-            
-     
