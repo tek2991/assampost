3a1bc4fe5c23ee4fc5fb759fc5722b30acc48b23
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-21 14:17:41 +0530

    Updated cal-action blade

--- resources/views/layouts/call-action.blade.php
+++ resources/views/layouts/call-action.blade.php
@@ -1,13 +1,8 @@
 <section class="call-to-action">
     <div class="container">
-        <div class="d-flex gap-4">
-            <div>
-                <h3>Need assistance with your article?</h3>
-            </div>
-            <div>
-                <a href="tel:1800 266 6868" class="call-to-action-btn">1800 266 6868</a>
-            </div>
-
+        <div class="d-flex gap-4 flex-wrap">
+            <h3>Need assistance with your article?</h3>
+            <a href="tel:1800 266 6868" class="call-to-action-btn">1800 266 6868</a>
         </div>
     </div>
 </section>
