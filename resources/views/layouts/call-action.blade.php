df1d716380006fa1159ab077adbcaedb619b320d
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-02-21 14:10:33 +0530

    Updated call-action blade

--- resources/views/layouts/call-action.blade.php
+++ resources/views/layouts/call-action.blade.php
@@ -1,13 +1,13 @@
-<section class="call-to-action"> 
-	<div class="container">
-			<div class="row">
-				<div class="col-lg-4 col-md-12">
-					<h3>Need assistance with your article?</h3>
-				</div>
-				<div class="col-lg-2 col-md-12">
-					<a href="tel:1800 266 6868" class="call-to-action-btn">1800 266 6868</a>
-				</div>
-				
-			</div>
-	</div>		
+<section class="call-to-action">
+    <div class="container">
+        <div class="d-flex gap-4">
+            <div>
+                <h3>Need assistance with your article?</h3>
+            </div>
+            <div>
+                <a href="tel:1800 266 6868" class="call-to-action-btn">1800 266 6868</a>
+            </div>
+
+        </div>
+    </div>
 </section>
