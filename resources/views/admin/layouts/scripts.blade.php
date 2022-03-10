bd3b52903ae0c39d1a8bf712bdbcf2aa9e5b6f56
wakibul <wakibulh@gmail.com>
2022-03-10 18:18:46 +0530

    audit

--- resources/views/admin/layouts/scripts.blade.php
+++ resources/views/admin/layouts/scripts.blade.php
@@ -1,5 +1,5 @@
 <!-- Bootstrap core JavaScript-->
-    <script src="{{asset('cms/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('assets/js/jquery-3.6.0.min.js')}}"></script>
     <script src="{{asset('cms/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Core plugin JavaScript-->
